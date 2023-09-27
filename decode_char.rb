3ce519e0fc1380a05c26c424aa18460b13a80d6f
Ahmidou EL mehdi <ahmidouem@gmail.com>
2023-09-28 00:19:18 +0100

    fix error relared Linters

--- decode_char.rb
+++ decode_char.rb
@@ -11,12 +11,14 @@ def decode_char(morse_code)
   # Return the decoded character or nil if not found
   morse_code_dict[morse_code]
 end
+
 # Define a method to decode a word in Morse code
 def decode_word(morse_word)
   characters = morse_word.split
   # Map each Morse character to its corresponding letter, and join them into a word
   characters.map { |char| decode_char(char) }.join
 end
+
 # Define a method to decode a message in Morse code
 def decode_message(morse_message)
   words = morse_message.split('   ') # NOTE: 3 spaces represent a word separation
