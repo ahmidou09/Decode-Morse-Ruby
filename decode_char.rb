cefd4cb20965f588b3562a062f160ecf49cee275
jlcarrascof <entrenamientospuntonet@gmail.com>
2023-09-27 19:08:16 -0400

    Document the source code that we developed

--- decode_char.rb
+++ decode_char.rb
@@ -1,3 +1,4 @@
+# Define a method to decode a single Morse code character
 def decode_char(morse_code)
   morse_code_dict = {
     '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
@@ -7,20 +8,24 @@ def decode_char(morse_code)
     '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
     '--..' => 'Z'
   }
-
+  # Return the decoded character or nil if not found
   morse_code_dict[morse_code]
 end
-
+# Define a method to decode a word in Morse code
 def decode_word(morse_word)
   characters = morse_word.split
+  # Map each Morse character to its corresponding letter, and join them into a word
   characters.map { |char| decode_char(char) }.join
 end
-
+# Define a method to decode a message in Morse code
 def decode_message(morse_message)
-  words = morse_message.split
+  words = morse_message.split('   ') # NOTE: 3 spaces represent a word separation
+  # Map each Morse word to its corresponding English word, and join them into a message
   words.map { |word| decode_word(word) }.join(' ')
 end
-
+# Define the Morse code message
 message = '--  -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
+# Decode the message
 decoded_message = decode_message(message)
+# Print the decoded message
 puts decoded_message
